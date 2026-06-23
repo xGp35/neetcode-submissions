4a8d436a984786bd5e2858233331e661f9fb6f59
Girija <78363935+xGp35@users.noreply.github.com>
2026-06-23 09:51:19 +0530

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        subsets, currSet = [], []
+        currSum = 0
+        candidates.sort()
+
+        def helper(i, currSet, currSum):
+            if currSum == target:
+                subsets.append(currSet.copy())
+                return
+            
+            if i >= len(candidates) or currSum > target:
+                return
+            
+            if currSum + candidates[i] > target:
+                return
+
+            currSet.append(candidates[i])
+            helper(i+1, currSet, currSum + candidates[i])
+            currSet.pop()
+
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i+=1
+            helper(i+1, currSet, currSum)
+        
+        helper(0, currSet, currSum)
+        return subsets
