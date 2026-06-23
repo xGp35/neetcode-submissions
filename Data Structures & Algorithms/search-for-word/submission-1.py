14ab4119bd0c9a4292595e38e3d81eacc3963b5a
Girija <78363935+xGp35@users.noreply.github.com>
2026-06-23 20:24:36 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+        visited = set()
+
+        def dfs(i, node):
+            if node in visited: return
+            row, col = node
+
+            if(row < 0 or col < 0 or row >= m or col >= n or board[row][col] != word[i]):
+                return
+            
+            if i == len(word) - 1:
+                return True
+
+            visited.add(node)
+
+            neighbors = [(row+1, col),(row, col+1),(row-1, col),(row, col-1)]
+
+            for nbr in neighbors:
+                if dfs(i + 1, nbr):
+                    return True
+            visited.remove(node)
+
+        for row in range(m):
+            for col in range(n):
+                if board[row][col] == word[0]:
+                    if dfs(0, (row, col)):
+                        return True
+        
+        return False
+
