4e43320b8747502d23296a9a1f87ba0d83ff64cb
Girija <78363935+xGp35@users.noreply.github.com>
2026-06-28 19:13:33 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        if n < 2: return 0
+
+        memo = [float('inf')]*(n+1)
+
+        memo[0] = 0
+        memo[1] = 0
+
+        for i in range(2, n+1):
+            memo[i] = min(cost[i-1] + memo[i-1], cost[i-2] + memo[i-2])
+
+        return memo[n]
