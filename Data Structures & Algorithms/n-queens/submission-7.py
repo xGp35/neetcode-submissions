233ea27ce589b5cc56533f43d83a6c3a8a4bd773
Girija <78363935+xGp35@users.noreply.github.com>
2026-08-08 09:44:16 +0530

    Add: n-queens - submission-7

---
+++ Data Structures & Algorithms/n-queens/submission-7.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        colSet = set()
+        negDiag = set()
+        posDiag = set()
+        
+        board = [['.']*n for _ in range(n)]
+        result = []
+        
+        def helper(r):
+            if r == n: # Add to board to the result
+                newBoard = ["".join(row) for row in board]
+                result.append(newBoard)
+                return
+
+            for c in range(n):
+                if c not in colSet and (r+c) not in posDiag and (r-c) not in negDiag:
+                    colSet.add(c)
+                    posDiag.add(r+c)
+                    negDiag.add(r-c)
+                    board[r][c] = 'Q'
+
+                    helper(r+1)
+
+                    colSet.remove(c)
+                    posDiag.remove(r+c)
+                    negDiag.remove(r-c)
+                    board[r][c] = '.'
+        
+        helper(0)
+        return result
+
