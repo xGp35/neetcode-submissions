af51bb82be2cff0fd38952e9bf7454a8c3e90fe1
Girija <78363935+xGp35@users.noreply.github.com>
2026-08-16 23:50:48 +0530

    Add: count-number-of-islands - submission-12

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-12.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        visited = set()
+
+        def explore(node):
+            if node in visited: return 
+            row,col = node
+            visited.add(node)
+
+            neighbors = [(row+1,col),(row, col+1),(row-1,col),(row,col-1)]
+
+            for nbr in neighbors:
+                r, c = nbr
+                if (0<=r<m and 0<=c<n and grid[r][c] == '1'):
+                    explore(nbr)
+        
+        count = 0
+        for row in range(m):
+            for col in range(n):
+                if (row,col) not in visited and grid[row][col] == '1':
+                    explore((row,col))
+                    count += 1
+        
+        return count
+
