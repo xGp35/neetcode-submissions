bc467463fc184b44c27fd6b436299f6a842b3b79
Girija <78363935+xGp35@users.noreply.github.com>
2026-08-25 00:16:29 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # Number of distinct combinations - not minimum
+        memo = {}
+        def dfs(i, target):
+            if (i, target) in memo: return memo[(i,target)]
+            if target < 0: return 0
+            if target == 0: return 1
+
+            ways = 0
+            for j in range(i, len(coins)):
+                remainder = target - coins[j]
+                ways += dfs(j, remainder)
+            memo[(i,target)] = ways
+            return ways
+        
+        return dfs(0, amount)
+
+            
+
+
