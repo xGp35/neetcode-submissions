0c6bb8f70abde10c106bd2428aec15a956d787f6
Girija <78363935+xGp35@users.noreply.github.com>
2026-09-03 15:25:49 +0530

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        curSet = []
+
+        def dfs(i):
+            if i == len(s): 
+                result.append(curSet.copy())
+                return
+
+            for j in range(i, len(s)):
+                if s[i:j+1] == s[i:j+1][::-1]:
+                    curSet.append(s[i:j+1])
+                    dfs(j+1)
+                    curSet.pop()
+        dfs(0)
+        return result
